f717d0e96d9907d43cbad2e8129100062a0aceae
anotherjsguy <kuldeepkeshwar@gmail.com>
2018-05-13 00:53:44 +0530

    fix event

--- src/switch.js
+++ src/switch.js
@@ -9,7 +9,6 @@ import React from 'react';
 // (if we've gotten to that part)
 class Switch extends React.Component {
   onChangeHandler = e => {
-    e.preventDefault();
     this.props.onClick();
   };
   render() {
